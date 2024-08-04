79eb90ae9610b3b922433d86f91eb406e7b42e22
Chukwuma Emmanuella <103493768+ch3lla@users.noreply.github.com>
2024-08-04 09:43:33 +0100

    css update

--- src/Components/Connected.jsx
+++ src/Components/Connected.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Connected = (props) => {
     return (
         <div className='connected-conatiner'>
-            <h1 className="connected-message">You are connected to metamask</h1>
-            <p className="connected-account">Metamask Account: {props.account}</p>
+            <h1 className="connected-account">You are connected to metamask</h1>
+            <p className="connected-contract-address">Metamask Account: {props.account}</p>
 
             { props.showButton ? (
-                <p className="connected_account">You have already voted</p>
+                <p className="connected-account">You have already voted</p>
             ): (
                 <div>
                     <input type="text" placeholder="Enter candidate name" value = {props.name} onChange={props.handleCandidateNameChange}></input>
